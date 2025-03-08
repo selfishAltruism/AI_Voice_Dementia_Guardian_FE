9233de005b776b8c9d8c449c4533c1671e17c35a
selfishAltruism <officialkyus@gmail.com>
2025-03-08 16:30:42 +0900

    chore: agreement page setting (#1)

---
+++ src/app/agreement/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+const Agreement = () => {
+  const [consents, setConsents] = useState({
+    consent1: false,
+    consent2: false,
+    consent3: false,
+  });
+
+  return <>Agreement</>;
+};
+
+export default Agreement;

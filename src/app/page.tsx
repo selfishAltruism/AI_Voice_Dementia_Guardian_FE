5450eb55a454d7c422b402fc22d61661b7a94e6f
selfishAltruism <officialkyus@gmail.com>
2025-03-17 17:07:16 +0900

    feat: home page update (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { AILoading } from "@/entities/inference";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex gap-8">
       <div className="flex items-center gap-2">
         <h1>AI</h1>
         <div className="max-w-[60px]">
@@ -13,8 +13,30 @@ export default function Home() {
         </div>
         <h1>치매 지킴이</h1>
       </div>
-      <p className="text-gray">~서비스에 대한 설명~</p>
+      <div className="h-[300px] w-[2px] rounded-md bg-litecore" />
+      <div className="text-start leading-relaxed text-gray">
+        <strong>AI 치매 지킴이</strong>는 음성 분석 AI 기술을 활용하여
+        <br />
+        간단한 대화를 통해 인지 기능을 점검할 수 있는 서비스 입니다. <br />
+        <div className="h-4" />
+        <strong className="text-xl">✅ 간편한 이용</strong>
+        <br />
+        버튼 클릭 한 번으로 시작 <br />
+        <div className="h-3" />
+        <strong className="text-xl">✅ 빠른 진행</strong>
+        <br />
+        n분 이내의 신속한 점검 <br />
+        <div className="h-3" />
+        <strong className="text-xl">✅ 정확한 결과</strong>
+        <br />
+        AI를 활용한 치매 위험군을 조기에 선별
+        <br />
+        <div className="h-4" />
+        <p className="text-lg font-bold">
+          👉 하단 시작 버튼을 통해 인지 건강을 손쉽게 확인하세요!
+        </p>
+      </div>
       <LinkButton title="AI 음성 치매 검사 시작" to="/personal-info" />
-    </>
+    </div>
   );
 }

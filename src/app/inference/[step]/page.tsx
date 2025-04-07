d5b992e498ba4e16089f75cc136305f0d1566898
selfishAltruism <officialkyus@gmail.com>
2025-04-07 13:32:54 +0900

    design: update inference page (#1)

--- src/app/inference/[step]/page.tsx
+++ src/app/inference/[step]/page.tsx
@@ -10,6 +10,8 @@ import {
   useInferenceInputDataStore,
 } from "@/shared/store";
 
+import { inferenceMainTitle } from "@/shared";
+
 const Inference = ({
   params: { step },
 }: {
@@ -25,16 +27,16 @@ const Inference = ({
 
   const router = useRouter();
 
-  useEffect(() => {
+  /* useEffect(() => {
     if (isPersonalInfoComplete()) return;
     toast.error("기본 인적 사항을 입력해주세요.");
     router.replace("/personal-info");
   }, []);
-
+ */
   return (
     <>
-      <h1 className="mt-20">들리는 문장을 듣고 따라 말해주세요.</h1>
-      <div className="mb-2 mt-1 h-[7px] w-[770px] rounded-lg bg-gradient-to-r from-sub to-main bg-[length:200%_200%]" />
+      <h1 className="mt-20 w-[750px]">{inferenceMainTitle[+step].child}</h1>
+      <div className="mb-2 mt-1 h-[7px] w-[750px] rounded-lg bg-gradient-to-r from-sub to-main bg-[length:200%_200%]" />
       <div className="mt-12 flex items-center gap-12">
         {isReplay.current ? (
           <Recorder isReplay={isReplay}>

99e431cada4bac2cf4d5b59a6f3c318433d91eb3
selfishAltruism <officialkyus@gmail.com>
2025-04-09 19:48:54 +0900

    design: update footer & link button position (#1)

--- src/entities/layout/ui/Footer.tsx
+++ src/entities/layout/ui/Footer.tsx
@@ -1,11 +1,11 @@
 export const Footer = () => (
   <footer className="p-4 text-xs">
-    <p className="mb-1 text-end">
+    <p className="mb-1 text-start">
       <strong>핏티</strong> 서울특별시 동작구 흑석로 84, 108관 202호
     </p>
-    <p className="mb-1 text-end">
+    <p className="mb-1 text-start">
       TEL 010-7162-5098 | E-MAIL ceo@fitrace.co.kr
     </p>
-    <p className="text-end">&copy; 2025 Team Fity. All rights reserved.</p>
+    <p className="text-start">&copy; 2025 Team Fity. All rights reserved.</p>
   </footer>
 );
